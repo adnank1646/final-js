672c44990d6ae3f95675b1d25f8c7d58b10aa637
adnank1646 <91988256+adnank1646@users.noreply.github.com>
2024-09-07 11:11:42 +0000

    Conversion

---
+++ 01_basics/03_convert.js
@@ -0,0 +1,29 @@
+let score = "55"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueinN = Number(score)
+//console.log(typeof valueinN)
+
+
+
+// "33" => 33
+// "33cvb" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+//""  => false
+//"adbab" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
