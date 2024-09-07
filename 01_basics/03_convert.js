3f45936be80c8b1c2f5b336b2ecb91efcd50641a
adnank1646 <91988256+adnank1646@users.noreply.github.com>
2024-09-07 15:11:08 +0000

    Config

--- 01_basics/03_convert.js
+++ 01_basics/03_convert.js
@@ -15,7 +15,7 @@ let valueinN = Number(score)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1 => true; 0 => false
 //""  => false
@@ -25,5 +25,48 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//*********** OPERATIONS *************/
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "adnan"
+let str2 = " Khan"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); //string first then all treat as a string   //122
+// console.log(1+2+"2");// if string last then first operation completed then string added  //32
+
+// console.log((3+4)*5%3);
+
+// console.log(+true); //gives 1
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
