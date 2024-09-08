19094a6e67378b023b301e4e2c3def0736d5b4a1
Adnan Khan <91988256+adnank1646@users.noreply.github.com>
2024-09-08 13:26:54 +0000

    Config

---
+++ 01_basics/06_nums.js
@@ -0,0 +1,44 @@
+const score = 500
+// console.log(score);
+
+
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const other = 123.6598
+
+// console.log(other.toPrecision(3));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+///++++++++++ MATHS +++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.max(4,5,9,8));
+// console.log(Math.min(4,5,9,8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+Math.random() * (max-min+1)
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
+
+
+
+
+
