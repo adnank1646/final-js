ad1b22c90630c4baea38689ae2f5ddb6a7f0db36
Adnan Khan <91988256+adnank1646@users.noreply.github.com>
2024-09-10 18:46:14 +0000

    Object

--- 02_basic/03_objects.js
+++ 02_basic/03_objects.js
@@ -10,6 +10,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name:"Adnan",  //key:value
     age:18,
+    [mySym]:"mykey1", //to use it as a symbol insert mySym in [] 
     "full":"khan",
     location:"Noida",
     email:"Adnan@123",
@@ -18,10 +19,26 @@ const JsUser = {
 
 }
 //how to access objects
-console.log(JsUser.email);
-console.log(JsUser["email"]); // right way or preffered way
-console.log(JsUser["full"]);
-console.log(JsUser.mySym);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]); // right way or preffered way
+// console.log(JsUser["full"]);
+// console.log(JsUser[mySym]);
+
+//way to define
+//1. const name = Symbol("key")
+//2. [name]:"value"
+//3. to access it use object[name]
+
+JsUser.email = "adnan@ret.com"
+Object.freeze(JsUser)
+JsUser.email = "addghj@321"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+
+console.log(JsUser.greeting);
 
 
 
