bf3fa86e87903f7d4bb56c4bff21c45588291593
Adnan Khan <91988256+adnank1646@users.noreply.github.com>
2024-09-14 20:22:23 +0000

    SWITCH

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3 //"String"
+
+switch (month) {
+    case 1: //"String"
+        console.log("January");
+        break;
+    case 2:
+        console.log("FEB");
+        break;
+    case 3:
+        console.log("MARCH");
+        break;
+    case 4:
+        console.log("APRIL");
+        break;
+
+    default:
+        console.log("default not match");
+        
+        break;
+}
