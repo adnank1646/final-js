03f5a0d42b165f0d472aaabcda337eedabfddeb0
Adnan Khan <91988256+adnank1646@users.noreply.github.com>
2024-09-19 19:53:41 +0000

    OK

---
+++ 05_iteration/six.js
@@ -0,0 +1,28 @@
+// const code = ["js","ruby","java","python","cpp"]
+
+
+// const values = code.forEach((item) => {
+//    // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+//const newNums = myNums.filter((num) => num > 4)
+//console.log(newNums);
+const newNums = []
+
+
+const newmk = myNums.filter( (num) => {
+    return num > 4
+})
+//console.log(newmk);
+
+// myNums.forEach((num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
